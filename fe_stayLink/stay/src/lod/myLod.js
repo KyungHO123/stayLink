8184b0ce11f4e0d8794cfa9251bff701b65074e6
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2025-01-30 23:00:18 +0900

    Update myLod.js

--- fe_stayLink/stay/src/lod/myLod.js
+++ fe_stayLink/stay/src/lod/myLod.js
@@ -37,6 +37,27 @@ function MyLodImg({ lodImg }) {
     console.log(lodImg);
     return (
         <div className='myLod-imgBox'>
+            {/*
+                숙소 관리
+                    숙소 이미지
+                        - 페이지 네이션 <, > 버튼
+                        - 이미지 삭제 버튼 (이미지 삭제시 바로 삭제)
+                            - DB에서 완전히 삭제 및 폴더에서도 삭제
+                        - 이미지 추가 버튼 (이미지 추가시 바로 추가)
+
+                    숙소 정보
+                        - 수정
+                            - 숙소명
+                            - 사업자 번호
+                            - 주소
+                            - 숙소정보 등
+                        - 삭제(숙소 등록 해제)
+                            - 숙소 삭제시 모든 정보 삭제
+                                - 이미지 포함 삭제
+                        
+
+
+                */}
             {lodImg && lodImg.length > 0 ? (
                 lodImg.map((img, index) => (
                     <img style={{ width: '300px', height: '300px' }}
